b65ddf15e743fb07ba0662f18d434578b1fa2f95
Alex Alonso <alex4551@gmail.com>
2017-04-12 17:52:01 +0800

    add graphql middleware

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
 const port = 4000;
+
+app.use('/graphql', expressGraphQL({
+    graphiql: true
+}));
+
 app.listen(port,()=>{
     console.log("Listenning port: " + port);
 })
